a35c22653271321bdce3eff6541afe768c3bceb8
SELIMCNR <btk02672@gmail.com>
2023-05-05 14:16:33 +0300

    Update binding and landscape layout

--- app/src/main/java/com/selimcinar/calculatorjava/MainActivity.java
+++ app/src/main/java/com/selimcinar/calculatorjava/MainActivity.java
@@ -2,31 +2,42 @@ package com.selimcinar.calculatorjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.selimcinar.calculatorjava.databinding.ActivityMainBinding;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
     //Global kapsamda tanımlamalar
+    private ActivityMainBinding binding;
     EditText number1Text;
     EditText number2Text;
     TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //İnflate xml ile kodları bağlar bu aşağıdaki kodları her aktivitede ekle
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         //Objeler tanımlandı ve idlere erişim sağlandı
-         number1Text = findViewById(R.id.number1Text);
-         number2Text = findViewById(R.id.number2Text);
-         resultText = findViewById(R.id.ResultText);
+         number1Text = binding.number1Text.findViewById(R.id.number1Text);
+         number2Text = binding.number2Text.findViewById(R.id.number2Text);
+         resultText = binding.ResultText.findViewById(R.id.ResultText);
 
     }
 
 
     //Buttonlarda onclicke atanmış değerlerle işlemler
     // herkese açık return yok adı sum (Görünüm tarafından çağrılacak)
+    @SuppressLint("SetTextI18n")
     public  void  sum(View view){
         //İf kontrolünde boş rakam girişi engellendi
         if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
@@ -34,13 +45,14 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             //Kullanıcıdan veri almak objelerde
-            Double number1 = Double.parseDouble(number1Text.getText().toString());
-            Double number2 = Double.parseDouble(number1Text.getText().toString());
-            Double result = number1 + number2;
+            double number1 = Double.parseDouble(number1Text.getText().toString());
+            double number2 = Double.parseDouble(number1Text.getText().toString());
+            double result = number1 + number2;
             //Double değeri otomatik stringe dönüştürdü
-            resultText.setText("Result " + result);
+            resultText.setText(Double.toString(result));
         }
     }
+    @SuppressLint("SetTextI18n")
     public void deduct(View view){
 //İf kontrolünde boş rakam girişi engellendi
         if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
@@ -48,13 +60,14 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             //Kullanıcıdan veri almak objelerde
-            Double number1 = Double.parseDouble(number1Text.getText().toString());
-            Double number2 = Double.parseDouble(number1Text.getText().toString());
-            Double result = number1 - number2;
+            double number1 = Double.parseDouble(number1Text.getText().toString());
+            double number2 = Double.parseDouble(number1Text.getText().toString());
+            double result = number1 - number2;
             //Double değeri otomatik stringe dönüştürdü
-            resultText.setText("Result " + result);
+            resultText.setText(Double.toString(result));
         }
     }
+    @SuppressLint("SetTextI18n")
     public void multiply(View view){
         //İf kontrolünde boş rakam girişi engellendi
         if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
@@ -62,13 +75,14 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             //Kullanıcıdan veri almak objelerde
-            Double number1 = Double.parseDouble(number1Text.getText().toString());
-            Double number2 = Double.parseDouble(number1Text.getText().toString());
-            Double result = number1 * number2;
+            double number1 = Double.parseDouble(number1Text.getText().toString());
+            double number2 = Double.parseDouble(number1Text.getText().toString());
+            double result = number1 * number2;
             //Double değeri otomatik stringe dönüştürdü
-            resultText.setText("Result " + result);
+            resultText.setText(Double.toString(result));
         }
     }
+    @SuppressLint("SetTextI18n")
     public void divide(View view){
         //İf kontrolünde boş rakam girişi engellendi
         if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
@@ -76,11 +90,11 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             //Kullanıcıdan veri almak objelerde
-            Double number1 = Double.parseDouble(number1Text.getText().toString());
-            Double number2 = Double.parseDouble(number1Text.getText().toString());
-            Double result = number1 / number2;
+            double number1 = Double.parseDouble(number1Text.getText().toString());
+            double number2 = Double.parseDouble(number1Text.getText().toString());
+            double result = number1 / number2;
             //Double değeri otomatik stringe dönüştürdü
-            resultText.setText("Result " + result);
+            resultText.setText(Double.toString(result));
         }
     }
 }
